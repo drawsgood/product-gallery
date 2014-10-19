d7ccd33a779ad9b088c3614095bea135a592bf63
drawsgood <mbmyers@gmail.com>
2014-10-19 00:01:58 -0500

    updated partials and settings.py

--- productgallery/urls.py
+++ productgallery/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, include, url
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 admin.autodiscover()
 
@@ -21,3 +24,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- products/models.py
+++ products/models.py
@@ -4,7 +4,7 @@ class Product(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
     colors = models.IntegerField()
-    image = models.ImageField(upload_to = 'static/products/img')
+    image = models.ImageField(upload_to = 'images/')
     #thumbnail = models.CharField(max_length=150, blank=True)
 
     def __str__(self):
